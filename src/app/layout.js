bfe4922f4879b4dab49e87ad17758ee6f78c4cf2
G-Anubhav <anubhavgoyal868@gmail.com>
2025-07-09 00:31:34 +0530

    New Commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,8 @@
 // src/app/layout.js
 import "../app/globals.css";
-import { Nunito_Sans } from 'next/font/google'; 
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
 import '@fortawesome/fontawesome-free/css/all.min.css';
-
-const nunitoSans = Nunito_Sans({
-  subsets: ['latin'],
-  weight: ['200', '400', '600', '700', '800', '1000'], // Match the weights you need
-  display: 'swap',
-})
 
 export const metadata = {
   title: "Indo Real Estate",
@@ -19,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={nunitoSans.className}>
+      <body>
         <Navbar />
         {children}
         <Footer />
